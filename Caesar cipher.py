d0d54c4af8ade4f48e5a92b8453213aa43c3579b
miholom <124650305+miholom@users.noreply.github.com>
2023-03-01 08:46:11 +0700

    заменил все вхождения функцией

--- Caesar cipher.py
+++ Caesar cipher.py
@@ -6,46 +6,37 @@ shift_input = int(input()) # int(27)
 strings = input().lstrip(' ').rstrip(' ') # "abc"
 string_on_exit = []
 d = ""
+ALPHABET_LEN = len(ALPHABET)
+
+# Заменил повторяющийся код функцией, вспомнил как список передать в функцию с помощью *
+def func(cezarPass, ALPHABET_LEN, *kwrg):
+        if cezarPass == ALPHABET_LEN:
+            string_on_exit.append(ALPHABET[cezarPass-ALPHABET_LEN])
+        elif cezarPass > ALPHABET_LEN:
+            string_on_exit.append(ALPHABET[cezarPass-ALPHABET_LEN])
+        elif cezarPass < ALPHABET_LEN:
+            string_on_exit.append(ALPHABET[cezarPass])
 
 # Запускаем цикл через который каждый символ в алфавите
 for i, string in enumerate(strings):
     a = ALPHABET.find(string)
+    cezarPass = a+shift_input
 
 # В первом блоке просматриваем вариант когда символ сдвигается в пределах алвафита    
-    cezarPass = a+shift_input
-    if 0 <= shift_input <= len(ALPHABET):
-        if cezarPass == len(ALPHABET):
-            string_on_exit.append(ALPHABET[cezarPass-len(ALPHABET)])
-        elif cezarPass > len(ALPHABET):
-            string_on_exit.append(ALPHABET[cezarPass-len(ALPHABET)])
-        elif cezarPass < len(ALPHABET):
-            string_on_exit.append(ALPHABET[cezarPass])
+    if 0 <= shift_input <= ALPHABET_LEN:
+        func(cezarPass, ALPHABET_LEN, *string_on_exit)
 
 # во втором блоке просматриваем вариант когда уходим в минус
     elif (0 > shift_input):
-        count = copy.deepcopy(shift_input)
-        while 0 > count:
-            count += len(ALPHABET)
-        cezarPass_copy = a + count
-        if cezarPass_copy == len(ALPHABET):
-            string_on_exit.append(ALPHABET[cezarPass_copy-len(ALPHABET)])
-        elif cezarPass_copy > len(ALPHABET):
-            string_on_exit.append(ALPHABET[cezarPass_copy-len(ALPHABET)])
-        elif cezarPass_copy < len(ALPHABET):
-            string_on_exit.append(ALPHABET[cezarPass_copy])
+        while 0 > cezarPass:
+            cezarPass += ALPHABET_LEN
+        func(cezarPass, ALPHABET_LEN, *string_on_exit)
 
 # в третьем блоке просматриваем вариант когда уходим в огромный плюс    
-    elif (shift_input > len(ALPHABET)):
-        count = copy.deepcopy(shift_input)
-        while count > len(ALPHABET):
-            count -= len(ALPHABET)
-        cezarPass_copy = a + count
-        if cezarPass_copy == len(ALPHABET):
-            string_on_exit.append(ALPHABET[cezarPass_copy-len(ALPHABET)])
-        elif cezarPass_copy > len(ALPHABET):
-            string_on_exit.append(ALPHABET[cezarPass_copy-len(ALPHABET)])
-        elif cezarPass_copy < len(ALPHABET):
-            string_on_exit.append(ALPHABET[cezarPass_copy])
+    elif (shift_input > ALPHABET_LEN):
+        while cezarPass > ALPHABET_LEN:
+            cezarPass -= ALPHABET_LEN
+        func(cezarPass, ALPHABET_LEN, *string_on_exit)
 
 # В конце через цикл for и формат выводим на печать получившееся решение
 for i in string_on_exit:
